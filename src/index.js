6ef7f75fe2bafd9d94d7e88f41334a8738db6404
sari <sari.korpialho@gmail.com>
2022-01-28 15:17:29 +0200

    update react package and start project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const App = () => {
+  const course = 'Superadvanced web and mobile programming'
+  const part1 = 'Basics of React'
+  const exercises1 = 8
+  const part2 = 'Using props'
+  const exercises2 = 10
+  const part3 = 'Component states'
+  const exercises3 = 12
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>{part1} {exercises1}</p>
+      <p>{part2} {exercises2}</p>
+      <p>{part3} {exercises3}</p>
+      <p>Total {exercises1 + exercises2 + exercises3} exercises</p>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
