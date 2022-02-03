234e7977a441b4409c3dc2d7c41907e7e669b2e9
sari <sari.korpialho@gmail.com>
2022-02-03 11:20:48 +0200

    Ready

--- src/index.js
+++ src/index.js
@@ -1,23 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (props) => {
+  
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+    )
+}
+const Courses = (props) =>{
+  return(
+    <div>
+      <li>{props.name} {props.exercises}</li>
+    </div>
+  )
+}
+const Contents = (props) => {
+  
+  return (
+    
+    <ul>
+		{props.parts.map(item => <Courses key={item.id} name={item.name} exercises={item.exercises}/>)}
+	  </ul>
+    )
+} 
+
+
+ 
 
 const App = () => {
   const course = 'Superadvanced web and mobile programming'
-  const part1 = 'Basics of React'
-  const exercises1 = 8
-  const part2 = 'Using props'
-  const exercises2 = 10
-  const part3 = 'Component states'
-  const exercises3 = 12
+  const parts = [
+    {
+      id: 1,
+      name: 'Basics of React',
+      exercises: 8
+    },
+    {
+      id: 2,
+      name: 'Using props',
+      exercises: 10
+    },
+    {
+      id: 3,
+      name: 'Component states',
+      exercises: 12
+    }
+  ]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
-      <p>Total {exercises1 + exercises2 + exercises3} exercises</p>
+      <Header title = {course}/> 
+      <Contents parts = {parts}/>
     </div>
   )
 }
